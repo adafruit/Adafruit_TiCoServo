c731b27aa8e0661fe5510b581eae63b189936e29
siddacious <nospam187+github@gmail.com>
2020-04-02 18:08:31 -0700

    actionified, formatted and doxy'd

--- Adafruit_TiCoServo.h
+++ Adafruit_TiCoServo.h
@@ -22,8 +22,8 @@
 #ifndef _ADAFRUIT_TICOSERVO_H_
 #define _ADAFRUIT_TICOSERVO_H_
 
-#include <Arduino.h>
 #include "known_16bit_timers.h" // Timer pins for various Arduino-alikes.
+#include <Arduino.h>
 // This header is used (instead of the stock Arduino digitalPinToTimer()
 // in pins_arduino.h) because the latter doesn't always give precedence
 // to 16-bit timers on certain pins; e.g. on Leonardo, pin 11 refers to
@@ -31,52 +31,63 @@
 // esoteric Arduino-compatibles may not work off the bat until this header
 // file is suitably amended.
 
-#ifdef TIMER1_A_PIN             // Defined in known_16bit_timers.h if present
- typedef uint16_t servoPos_t;   // Servo positions are 16-bit values
- // Min/max pulse range is the same as stock Arduino Servo library:
- #define MIN_PULSE_WIDTH  544   // Shortest pulse sent to a servo (microsec)
- #define MAX_PULSE_WIDTH 2400   // Longest pulse sent to a servo (microsec)
-#else                           // No 16-bit timers, presumably ATtiny device
- typedef uint8_t servoPos_t;    // Servo positions are 8-bit values
- #define TIMER1_A_PIN 1
- #define TIMER1_B_PIN 4
- #define __TINY_SERVO__
+#ifdef TIMER1_A_PIN          // Defined in known_16bit_timers.h if present
+typedef uint16_t servoPos_t; // Servo positions are 16-bit values
+// Min/max pulse range is the same as stock Arduino Servo library:
+#define MIN_PULSE_WIDTH 544  // Shortest pulse sent to a servo (microsec)
+#define MAX_PULSE_WIDTH 2400 // Longest pulse sent to a servo (microsec)
+#else                        // No 16-bit timers, presumably ATtiny device
+typedef uint8_t servoPos_t; // Servo positions are 8-bit values
+#define TIMER1_A_PIN 1
+#define TIMER1_B_PIN 4
+#define __TINY_SERVO__
 #endif
-
+/**
+ * @brief Class for controlling servos while using the Adafruit NeoPixel library
+ *
+ */
 class Adafruit_TiCoServo {
- public:
+public:
 #ifdef __TINY_SERVO__
   // Behavior is slightly different for ATtiny vs other devices.
   // Due to the limited timer resolution, servo read/write only work in
   // 'raw' timer/counter ticks; degrees or microseconds are never used.
-  Adafruit_TiCoServo(void) : pin(-1), on(false) { };
-  void              attach(const int8_t pin),
-                    write(const servoPos_t pos);
+  Adafruit_TiCoServo(void) : pin(-1), on(false){};
+  void attach(const int8_t pin), write(const servoPos_t pos);
+  /**
+   * @brief Return servo position
+   * @return The servo position
+   */
   inline servoPos_t read(void) __attribute__((always_inline)) { return *ocr; }
 #else
   // On other boards, read/write are available both in degrees and
   // microseconds, consistent with original Arduino Servo library.
-  Adafruit_TiCoServo(void) : pin(-1), on(false), minPulse(MIN_PULSE_WIDTH),
-    maxPulse(MAX_PULSE_WIDTH) { };
-  void        attach(const int8_t pin, const uint16_t min = MIN_PULSE_WIDTH,
-                const uint16_t max = MAX_PULSE_WIDTH),
-              write(const uint16_t pos);
-  uint8_t     read(void);
-  uint16_t    readMicroseconds(void);
+  Adafruit_TiCoServo(void)
+      : pin(-1), on(false), minPulse(MIN_PULSE_WIDTH),
+        maxPulse(MAX_PULSE_WIDTH){};
+  void attach(const int8_t pin, const uint16_t min = MIN_PULSE_WIDTH,
+              const uint16_t max = MAX_PULSE_WIDTH),
+      write(const uint16_t pos);
+  uint8_t read(void);
+  uint16_t readMicroseconds(void);
 #endif
-  void        detach(void);
+  void detach(void);
+  /**
+   * @brief Get the attachment state
+   * @return bool true: a pin is attached false: no pin is attached
+   */
   inline bool attached(void) __attribute__((always_inline)) { return on; }
 
- private:
-  int8_t               pin;          // Arduino pin #, or -1 if invalid
-  volatile servoPos_t *ocr;          // Output compare register for pin
-#ifdef TIMER3_A_PIN                  // For chips with >1 timer/counter...
-  volatile servoPos_t *counter;      // Timer/Counter count reg. for pin
-#endif                               // (else always TCNT1)
-  boolean              on;           // True after first write() call
-  void                 toggle(void); // PWM on/off
+private:
+  int8_t pin;                   // Arduino pin #, or -1 if invalid
+  volatile servoPos_t *ocr;     // Output compare register for pin
+#ifdef TIMER3_A_PIN             // For chips with >1 timer/counter...
+  volatile servoPos_t *counter; // Timer/Counter count reg. for pin
+#endif                          // (else always TCNT1)
+  boolean on;                   // True after first write() call
+  void toggle(void);            // PWM on/off
 #ifndef __TINY_SERVO__
-  uint16_t             minPulse, maxPulse;
+  uint16_t minPulse, maxPulse;
 #endif
 };
 
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-Adafruit_TiCoServo
-==================
+# Adafruit TiCoServo [![Build Status](https://github.com/adafruit/Adafruit_TiCoServo/workflows/Arduino%20Library%20CI/badge.svg)](https://github.com/adafruit/Adafruit_TiCoServo/actions)
+
+This is the Adafruit TiCoServo Arduino Library for Arduino
 
 An alternate servo library when NeoPixels and servos are both needed in the same sketch (the stock Arduino Servo library stutters with the addition of NeoPixels). Has some limitations of its own, see examples for pros and cons. See Adafruit_TiCoServo.cpp for attribution.
 
@@ -11,3 +12,42 @@ Compatible pins are determined by the specific hardware.
 * Adafruit Flora: pins D9, D10. 
 * PJRC Teensy 2.0 (not Teensy++ or 3.X): pins 4, 9, 14, 15.
 * Arduino Mega: pins 2, 3, 5, 6, 7, 8, 11, 12, 13, 44, 45, 46.
+
+
+
+Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
+
+# Dependencies
+[Adafruit NeoPixel](https://github.com/adafruit/Adafruit_NeoPixel)
+
+# Contributing
+
+Contributions are welcome! Please read our [Code of Conduct](https://github.com/adafruit/Adafruit_TiCoServo/blob/master/CODE_OF_CONDUCT.md>)
+before contributing to help this project stay welcoming.
+
+## Documentation and doxygen
+Documentation is produced by doxygen. Contributions should include documentation for any new code added.
+
+Some examples of how to use doxygen can be found in these guide pages:
+
+https://learn.adafruit.com/the-well-automated-arduino-library/doxygen
+
+https://learn.adafruit.com/the-well-automated-arduino-library/doxygen-tips
+
+
+Written by Phil Burgess for Adafruit Industries, incorporating work
+by Paul Stoffregen, Jesse Tane, Jérôme Despatis, Michael Polli and
+Dan Clemens. The file 'known_16bit_timers.h' is taken verbatim from
+Paul Stoffregen's fork of the TimerOne library:
+https://github.com/PaulStoffregen/TimerOne
+and this code is provided under the same "CC BY 3.0 US" license terms.
+
+Open Source License
+
+Adafruit_TiCoServo is free software. You can redistribute it and/or
+modify it under the terms of Creative Commons Attribution 3.0 United
+States License. To view a copy of this license, visit
+http://creativecommons.org/licenses/by/3.0/us/
+
+
+To install, use the Arduino Library Manager and search for "Adafruit TiCoServo" and install the library.
